c56364f247e7686e82f19556cd72138181c5a71f
Ryan_rlm <ryan_rlm@foxmail.com>
2022-09-21 15:55:01 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -4,11 +4,11 @@ import org.litepal.crud.DataSupport;
 
 public class City extends DataSupport {
 
-    private  int id;
+    private int id;
 
-    private  String cityName;
+    private String cityName;
 
-    private  int cityCode;
+    private int cityCode;
 
     private int provinceId;
 
